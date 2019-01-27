4ac51b7f023eb83276ac491a36ee804ad2ce5e98
manoj <manoj@ManojWindows10DeskTop.fios-router.home>
2019-01-26 22:25:45 -0500

    Session 2 - Arithmetic

---
+++ Session2/src/Arithmetic.java
@@ -0,0 +1,16 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int b = 2;
+		
+		int total = i + b;
+		
+		System.out.println(i + " + " + b + " = " + total);
+		
+		
+	}
+
+}
